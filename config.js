e94a2e98a49f19ad89df378ab9243fc655da06ec
Ifung Tech Dev <150768783+ifungtech@users.noreply.github.com>
2024-03-02 12:27:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-global.owner = ['6285842647866']  
-global.mods = ['6285842647866'] 
-global.prems = ['6285842647866']
-global.nameowner = 'Lann'
-global.numberowner = '6285842647866'
+global.owner = ['6285931969956']  
+global.mods = ['6285931969956'] 
+global.prems = ['6285931969956']
+global.nameowner = 'IFTXH'
+global.numberowner = '6285931969956'
 global.mail = 'support@tioprm.eu.org' 
 global.gc = 'https://chat.whatsapp.com/G4f1fTpz9zL4EH3FyIcaPR'
 global.instagram = 'https://instagram.com/erlanrahmat_14'
